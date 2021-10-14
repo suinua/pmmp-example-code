b5a8edfa9c1887cac38aba1149a4eef089fe286e
suinua <maatinosippo@gmail.com>
2021-10-14 23:46:40 +0900

    test

--- bin/main.dart
+++ bin/main.dart
@@ -9,8 +9,10 @@ import 'package:markdown/markdown.dart';
 import 'article.dart';
 
 void main() {
-  print(Platform.environment['PATH']);
-  print(Platform.script.path);
+  //print(Platform.environment['KEY']);
+  var path = Platform.script.path.replaceFirst('bin/main.dart', 'markdown/');
+  print(path);
+  print(File(path).exists());
   //convertMarkdownToHtml();
 }
 
